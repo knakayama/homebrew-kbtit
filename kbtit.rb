70e6e3c8d4318e5e91b27749d7c819257ea8fa36
knakayama <knakayama.sh@gmail.com>
2015-05-01 21:13:16 +0900

    ﾁｭﾊﾟﾊﾟッ

---
+++ kbtit.rb
@@ -0,0 +1,10 @@
+class Kbtit < Formula
+  homepage 'https://github.com/knakayama/kbtit'
+  url 'https://github.com/knakayama/kbtit/archive/v0.0.1.tar.gz'
+  sha256 'd38d24ccf017e32cd9704d9289a118974197054a28d54c9b23f272e5de53ad6f'
+  version '0.0.1'
+
+  def install
+    bin.install 'kbtit'
+  end
+end
