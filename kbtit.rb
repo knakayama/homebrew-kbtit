1af6388dcf944eb204e78687be1b25cca7deac47
knakayama <knakayama.sh@gmail.com>
2015-05-01 21:29:28 +0900

    ペンギンちゃんよお前よぉ！（意味不明）

--- kbtit.rb
+++ kbtit.rb
@@ -1,7 +1,7 @@
 class Kbtit < Formula
   homepage 'https://github.com/knakayama/kbtit'
   url 'https://github.com/knakayama/kbtit/archive/v0.0.2.tar.gz'
-  sha256 'd38d24ccf017e32cd9704d9289a118974197054a28d54c9b23f272e5de53ad6f'
+  sha256 '4737f2dad318ba095d1f7ee33df3a42f92b68062f908c01a194f418bc682fd8b'
   version '0.0.2'
 
   def install
